cfcb33649e1e1b1ed5bd373d0b0dcb561835fc3a
TeaFTI <TeaFTI@Gmail.com>
2024-07-15 10:14:10 -0700

    Revert previous update

--- tealogger/test/test_tealogger_package.py
+++ tealogger/test/test_tealogger_package.py
@@ -11,33 +11,33 @@ import tealogger
 class TestTeaLoggerPackage:
     """Test Tea Logger Package"""
 
-    def test_tealogger_instance(
-        self,
-    ):
-        """Test Tea Logger Instance"""
-
-        tealogger_a1 = tealogger.TeaLogger('Alpha')
-        tealogger_a2 = tealogger.TeaLogger('Alpha')
-
-        print(hex(id(tealogger_a1)))
-        print(hex(id(tealogger_a2)))
-
-        assert tealogger_a1 == tealogger_a2
-
-    # def test_tealogger_import(
+    # def test_tealogger_instance(
     #     self,
-    #     attribute: str,
     # ):
-    #     """Test tealogger Construction"""
+    #     """Test Tea Logger Instance"""
 
-    #     tealogger.log(tealogger.DEBUG, 'TeaLogger: Debug Message')
-    #     tealogger.debug('TeaLogger: Debug Message')
-    #     tealogger.info('TeaLogger: Info Message')
-    #     tealogger.warning('TeaLogger: Warning Message')
-    #     tealogger.error('TeaLogger: Error Message')
-    #     tealogger.critical('TeaLogger: Critical Message')
+    #     tealogger_a1 = tealogger.TeaLogger('Alpha')
+    #     tealogger_a2 = tealogger.TeaLogger('Alpha')
 
-    #     assert hasattr(tealogger, attribute)
+    #     print(hex(id(tealogger_a1)))
+    #     print(hex(id(tealogger_a2)))
+
+    #     assert tealogger_a1 == tealogger_a2
+
+    def test_tealogger_import(
+        self,
+        attribute: str,
+    ):
+        """Test tealogger Construction"""
+
+        tealogger.log(tealogger.DEBUG, 'TeaLogger: Debug Message')
+        tealogger.debug('TeaLogger: Debug Message')
+        tealogger.info('TeaLogger: Info Message')
+        tealogger.warning('TeaLogger: Warning Message')
+        tealogger.error('TeaLogger: Error Message')
+        tealogger.critical('TeaLogger: Critical Message')
+
+        assert hasattr(tealogger, attribute)
 
     # def test_debug_log(
     #     self,
